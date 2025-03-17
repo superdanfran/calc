36abd9a9d39a91705da8f87f2d38676ec516c7d2
TerraPrograms <mehdi_bouazra@mail.de>
2025-03-17 14:40:23 +0100

    winona split cleanup

--- calc/src/data/species.ts
+++ calc/src/data/species.ts
@@ -9179,7 +9179,7 @@ const SV_PATCH: {[name: string]: DeepPartial<SpeciesData>} = {
     abilities: {0: 'Cud Chew'},
   },
   Fezandipiti: {
-    types: ['Poison', 'Psychic'],
+    types: ['Poison', 'Fairy'],
     bs: {hp: 88, at: 91, df: 82, sa: 70, sd: 125, sp: 99},
     weightkg: 12.2,
     gender: 'M',
